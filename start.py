3ad67fa543c44ab83d17770d29b694c31c0e6439
jrpedersen <jesper2@peders1.dk>
2022-06-22 16:57:13 +0200

    Hardcoded a bound very dependent on the specific architecture

--- start.py
+++ start.py
@@ -28,8 +28,11 @@
 # import torchvision.models as models
 #from collections.abc import Mapping
 from typing import Optional
+from collections import OrderedDict
+from functools import partial
+
 import torch
-from torch import nn, optim
+from torch import nn, optim, linalg
 
 import torchmetrics
 
@@ -70,7 +73,7 @@ class MNISTDataModule(pl.LightningDataModule):
             self.mnist_predict = MNIST(self.data_dir, train=False, transform=self.transform)
 
     def train_dataloader(self):
-        return DataLoader(self.mnist_train, batch_size=32)
+        return DataLoader(self.mnist_train, batch_size=1)
 
     def val_dataloader(self):
         return DataLoader(self.mnist_val, batch_size=32)
@@ -86,20 +89,21 @@ class MNISTDataModule(pl.LightningDataModule):
 
 # class model_factory
 class SimpleMLP(pl.LightningModule):
-    def __init__(self, hidden_layer_config):
+    def __init__(self, hidden_layer_config, wealy_relu_neg_slope=0.01):
         super().__init__()
-        layers = []
         next_layer_input = 784
-        for hidden_layer in hidden_layer_config:
+        #layers = []
+        layers = OrderedDict()
+        for _i, hidden_layer in enumerate(hidden_layer_config):
             # Create layer
-            layers.append(
-                nn.Linear(in_features=next_layer_input, out_features=hidden_layer)
-            )
-            layers.append(nn.LeakyReLU())
+            # layers.append(nn.Linear(in_features=next_layer_input, out_features=hidden_layer))
+            # layers.append(nn.LeakyReLU(negative_slope=wealy_relu_neg_slope))
+            layers.update({'fc'+ str(_i): nn.Linear(in_features=next_layer_input, out_features=hidden_layer)})
+            layers.update({'af'+ str(_i): nn.LeakyReLU(negative_slope=wealy_relu_neg_slope)})
             # Update input size
             next_layer_input = hidden_layer
 
-        self.layers = nn.Sequential(*layers)
+        self.layers = nn.Sequential(layers)
 
         self.criterium = nn.CrossEntropyLoss(label_smoothing= 0.1)
 
@@ -140,17 +144,81 @@ class SimpleMLP(pl.LightningModule):
 
         #return {"loss": loss, "pred": max_pred}
 
+# function for MLP bound
+def mlp_term_bound(mlp_layer):
+    weight_spectral_norm = linalg.matrix_norm(mlp_layer.weight, ord=2)
+    h_n = linalg.vector_norm(1)
+    jac_h_n =linalg.matrix_norm(1, ord='fro')
+    return (1 + h_n**2) / (weight_spectral_norm**2 * jac_h_n**2)
+
+
+
+visualisation = {}
+def named_hook(m,i,o, name=None):
+    #print(type(i))
+    #for elem in i: print(type(elem))
+    #print(i)
+    visualisation[name] = i
+
+def hook_fn(m, i, o):
+    visualisation[m] = i
+
+def get_all_layers(net):
+    for name, layer in net._modules.items():
+    #If it is a sequential, don't register a hook on it
+    # but recursively register hook on all it's module children
+        if isinstance(layer, nn.Sequential):
+            get_all_layers(layer)
+        else:
+        # it's a non sequential. Register a hook
+            hook = partial(named_hook, name=name)
+            layer.register_forward_hook(hook)
+            # layer.register_forward_hook(hook_fn)
+
+# function for residual MLP bound
 
 # class bound_evaluator
 
 # class residual_bound_evaluator
+
+from functorch import jacrev
+
+def get_hn_and_jhn(model, x):
+    h_0 = x.view(1,-1)
+    jh_0 = torch.eye(h_0.shape[1]).unsqueeze(0)
+
+    h_n = [h_0]
+    jh_n = [jh_0]
+
+    for n in range(1,3):
+        partial_net =nn.Sequential(OrderedDict(list(model.layers._modules.items())[:(n*2)]))
+        h_n.append(partial_net(h_0))
+        jh_n.append(torch.squeeze(jacrev(partial_net)(h_0)))
+    return h_n, jh_n
+
+
+def mlp_bound(model, x):
+    h_n, jh_n = get_hn_and_jhn(model, x)
+    params = [param for (i,param) in enumerate(model.parameters()) if i%2==0]
+    bound = 0
+    for w, h, jh in zip(params,h_n,jh_n):
+        bound += (
+            (1 + linalg.vector_norm(h)**2) /
+            (linalg.matrix_norm(w, ord=2)**2 *
+            linalg.matrix_norm(jh, ord='fro')**2)
+        )
+    return bound
+#a=nn.Sequential(OrderedDict(list(model.layers._modules.items())[:4]))
+
+#ft_jacobian = jacrev(predict, argnums=2)(weight, bias, x)
+
+#jacrev(a)(inputs.view(1,-1))
 
 def train_one_epoch(training_loader, model, loss_fn,optimizer, epoch_index, tb_writer):
     running_loss = 0.
     last_loss = 0.
     for i, data in enumerate(training_loader):
         inputs, labels = data
-        pdb.set_trace()
         inputs.requires_grad_(True)
         inputs.retain_grad()
 
@@ -161,16 +229,29 @@ def train_one_epoch(training_loader, model, loss_fn,optimizer, epoch_index, tb_w
 
         loss.backward()
 
-        
+
         norm_grad_wrt_input = (inputs.grad**2).sum()
         norm_grad_wrt_weights = torch.tensor([(param.grad**2).sum() for param in model.parameters()]).sum()
+        #
+        #test = torch.autograd.functional.jacobian(model, inputs)
+        # def cfnew(f,g): return lambda x : f(g(x))
+        #  torch.autograd.functional.jacobian(cfnew(partial(loss_fn,target=labels), model), inputs)
+        # [torch.allclose(a,b) for (a,b) in zip(h_n, visualisation.values()[0:6:2])]
+        inputs.requires_grad_(False)
+
+        #assert [torch.allclose(a,b[0]) for (a,b) in zip(h_n, list(visualisation.values())[0:6:2])]
+
+        h_n, jh_n = get_hn_and_jhn(model, inputs)
+
+        bound = mlp_bound(model,inputs)
+        print('f in', norm_grad_wrt_input)
+        print('bound', bound)
+        print('f in * bound', norm_grad_wrt_input*bound)
+        print('f param', norm_grad_wrt_weights)
+        #assert all([torch.allclose(a,b[0]) for (a,b) in zip(h_n, list(visualisation.values())[0:6:2])])
+
 
         pdb.set_trace()
-
-        input.requires_grad_(False)
-
-
-
         optimizer.step()
         # Gather data and report
         running_loss += loss.item()
@@ -186,10 +267,12 @@ def train_one_epoch(training_loader, model, loss_fn,optimizer, epoch_index, tb_w
 
 def main_manual_train():
     mnist = MNISTDataModule(data_dir="./data")
-    simple_mlp = SimpleMLP([40,40,10])  # this is our LightningModule
+    simple_mlp = SimpleMLP([40,40,10], 0.01)
+    get_all_layers(simple_mlp)
     logger = TensorBoardLogger('lightning_logs/', name='my_model')
     mnist.setup()
     optimizer = optim.Adam(simple_mlp.parameters(), lr=1e-3)
+
     train_one_epoch(mnist.train_dataloader(), simple_mlp, nn.CrossEntropyLoss(label_smoothing=0.1), optimizer, 1, None)
 
 def main():
