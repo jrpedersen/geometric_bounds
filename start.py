a1a979cb3bfc59685bb548b72f69e06eabcdb0d9
jrpedersen <jesper2@peders1.dk>
2022-06-23 15:07:05 +0200

    Removed old hooks to utils script.

--- start.py
+++ start.py
@@ -56,28 +56,6 @@ from data.mnist import MNISTDataModule
 
         #return {"loss": loss, "pred": max_pred}
 
-visualisation = {}
-def named_hook(m,i,o, name=None):
-    #print(type(i))
-    #for elem in i: print(type(elem))
-    #print(i)
-    visualisation[name] = i
-
-def hook_fn(m, i, o):
-    visualisation[m] = i
-
-def get_all_layers(net):
-    for name, layer in net._modules.items():
-    #If it is a sequential, don't register a hook on it
-    # but recursively register hook on all it's module children
-        if isinstance(layer, nn.Sequential):
-            get_all_layers(layer)
-        else:
-        # it's a non sequential. Register a hook
-            hook = partial(named_hook, name=name)
-            layer.register_forward_hook(hook)
-            # layer.register_forward_hook(hook_fn)
-
 # function for residual MLP bound
 
 # class bound_evaluator
