8f7e4214a844b3f8a3f5c57093ec06bc39c2216b
jrpedersen <jesper2@peders1.dk>
2022-06-28 12:18:28 +0200

    Created both tight an paper bound for both mlp and mlp with skip. Still needs test to check that the bound is implemented correctly.

--- start.py
+++ start.py
@@ -49,26 +49,76 @@ from torchvision import transforms
 from models.simple_mlp import SimpleMLP
 from models.skip_mlp import SkipMLP
 from data.mnist import MNISTDataModule
-
-
-# class simpler dataset
-        #self.criterion(self(inputs), labels)
-
-        #return {"loss": loss, "pred": max_pred}
-
-# function for residual MLP bound
-
-# class bound_evaluator
-
-# class residual_bound_evaluator
+from utils import get_all_layers
 
 from functorch import jacrev
 
+def calc_bound(w, h, jh):
+    return (
+        (1 + linalg.vector_norm(h)**2) /
+        (linalg.matrix_norm(w, ord=2)**2 *
+        linalg.matrix_norm(jh, ord='fro')**2)
+    )
+
+def calc_tight_bound(w, h, jh):
+    return (
+        (1 + linalg.vector_norm(h)**2) /
+        (linalg.matrix_norm(torch.matmul(w, jh), ord='fro')**2)
+    )
+
+
+def mlp_bound(weight_layer, partial_net, x, bound_f = calc_tight_bound):
+    w = weight_layer.weight
+    h_0 = x.view(1,-1)
+
+    if len(partial_net) > 0:
+        h_n = partial_net(h_0)
+        jh_n = torch.squeeze(jacrev(partial_net)(h_0))
+    else:
+        h_n = h_0
+        jh_n = torch.eye(h_0.shape[1]).unsqueeze(0)
+    return bound_f(w, h_n, jh_n), (w,h_n,jh_n)
+
+def skip_mlp_bound(weight_layer, partial_net, x, negative_slope, bound_f = calc_tight_bound):
+    w_base = weight_layer.weight
+
+    h_0 = x.view(1,-1)
+    h_n = partial_net(h_0)
+    jh_n = torch.squeeze(jacrev(partial_net)(h_0))
+    pdb.set_trace()
+    w_skip =torch.eye(*w_base.shape) * (torch.gt(-weight_layer(h_n), 0) * negative_slope**(-1))
+    w = w_base + w_skip
+
+    return bound_f(w, h_n, jh_n), (w,h_n,jh_n)
+
+
+
+def get_bounds(model, x):
+    modules_list = list(model.layers._modules.items())
+    layer_wise_bound = []
+    for i in range(len(modules_list)):
+        name, layer = modules_list[i]
+
+        if name[:2] == 'fc':
+            partial_net = nn.Sequential(OrderedDict(modules_list[:i]))
+            weight_layer = layer
+            bound, _ = mlp_bound(weight_layer, partial_net, x)
+
+        elif name[:2] == 'sk':
+            partial_net = nn.Sequential(OrderedDict(modules_list[:i]))
+            weight_layer = layer.fc
+            bound, _ = skip_mlp_bound(weight_layer, partial_net, x, layer.activation.negative_slope)
+        else:
+            continue
+        layer_wise_bound.append(bound)
+    return torch.tensor(layer_wise_bound)
+
+
 def get_hn_and_jhn(model, x):
     h_0 = x.view(1,-1)
-    jh_0 = torch.eye(h_0.shape[1]).unsqueeze(0)
 
     h_n = [h_0]
+    jh_0 = torch.eye(h_0.shape[1]).unsqueeze(0)
     jh_n = [jh_0]
 
     for n in range(1,3):
@@ -77,7 +127,7 @@ def get_hn_and_jhn(model, x):
         jh_n.append(torch.squeeze(jacrev(partial_net)(h_0)))
     return h_n, jh_n
 
-def mlp_bound(model, x):
+def old_mlp_bound(model, x):
     h_n, jh_n = get_hn_and_jhn(model, x)
     params = [param for (i,param) in enumerate(model.parameters()) if i%2==0]
     bound = 0
@@ -115,8 +165,6 @@ def train_one_epoch(training_loader, model, loss_fn,optimizer, epoch_index, tb_w
 
 
         loss.backward()
-
-
         norm_grad_wrt_input = (inputs.grad**2).sum()
         norm_grad_wrt_weights = torch.tensor([(param.grad**2).sum() for param in model.parameters()]).sum()
         #
@@ -127,21 +175,22 @@ def train_one_epoch(training_loader, model, loss_fn,optimizer, epoch_index, tb_w
         inputs.requires_grad_(False)
 
         #assert [torch.allclose(a,b[0]) for (a,b) in zip(h_n, list(visualisation.values())[0:6:2])]
+        if 0:
+            h_n, jh_n = get_hn_and_jhn(model, inputs)
+            bound = old_mlp_bound(model,inputs)
+            tight_bound=tighter_mlp_bound(model,inputs)
+            print('f in', norm_grad_wrt_input)
+            print('bound', bound)
+            print('bound2', tight_bound)
+            print('f in * bound', norm_grad_wrt_input*bound)
+            print('Tighter f in * bound', norm_grad_wrt_input*tight_bound)
+            print('f param', norm_grad_wrt_weights)
+            #assert all([torch.allclose(a,b[0]) for (a,b) in zip(h_n, list(visualisation.values())[0:6:2])])
 
-        h_n, jh_n = get_hn_and_jhn(model, inputs)
-
-        bound = mlp_bound(model,inputs)
-        tight_bound=tighter_mlp_bound(model,inputs)
-        print('f in', norm_grad_wrt_input)
-        print('bound', bound)
-        print('bound2', tight_bound)
-        print('f in * bound', norm_grad_wrt_input*bound)
-        print('Tighter f in * bound', norm_grad_wrt_input*tight_bound)
-        print('f param', norm_grad_wrt_weights)
-        #assert all([torch.allclose(a,b[0]) for (a,b) in zip(h_n, list(visualisation.values())[0:6:2])])
-
-
-
+        test = get_bounds(model,inputs)
+        print('\n\n new way to calc bounds')
+        print(test)
+        print(test.sum())
         pdb.set_trace()
         optimizer.step()
         # Gather data and report
@@ -157,10 +206,11 @@ def train_one_epoch(training_loader, model, loss_fn,optimizer, epoch_index, tb_w
 
 
 def main_manual_train():
-    mnist = MNISTDataModule(data_dir="./data")
-    #simple_mlp = SimpleMLP([40,40,40,40,10], 0.01)
-    simple_mlp = SkipMLP([40,40,40,40,10], 0.01)
-    get_all_layers(simple_mlp)
+    mnist = MNISTDataModule(data_dir="./data", batch_size=1)
+    weakly_relu_neg_slope = 0.01
+    #simple_mlp = SimpleMLP([40,40,10], 0.01)
+    simple_mlp = SimpleMLP([40,40,40,40,10], 0.01)
+    #simple_mlp = SkipMLP([40,40,40,40,10], weakly_relu_neg_slope)
     logger = TensorBoardLogger('lightning_logs/', name='my_model')
     mnist.setup()
     optimizer = optim.Adam(simple_mlp.parameters(), lr=1e-3)
