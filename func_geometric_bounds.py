5c31532b2972edbf14cf0f91625d03b258868cf1
jrpedersen <jesper2@peders1.dk>
2022-06-29 15:25:36 +0200

    Refactored code to move the bounds to func_geometric_bounds.

---
+++ func_geometric_bounds.py
@@ -0,0 +1,87 @@
+
+from collections import OrderedDict
+from functools import partial
+import torch
+from torch import nn, optim, linalg
+from functorch import jacrev, vmap
+
+def calc_bound(w, h, jh):
+    return (
+        (1 + linalg.vector_norm(h, dim=1)**2) /
+        (linalg.matrix_norm(w, ord=2)**2 *
+        linalg.matrix_norm(jh, ord='fro')**2)
+    )
+
+def calc_tight_bound(w, h, jh):
+    return (
+        (1 + linalg.vector_norm(h, dim=1)**2) /
+        (linalg.matrix_norm(torch.bmm(w, jh), ord='fro')**2)
+    )
+
+def mlp_bound(weight_layer, partial_net, x, bound_f = calc_tight_bound):
+    h_0 = x.view(x.shape[0],-1)
+    if len(partial_net) > 0:
+        h_n = partial_net(h_0)
+        jh_n = vmap(jacrev(partial_net))(h_0)
+    else:
+        h_n = h_0
+        jh_n = torch.eye(h_0.shape[1]).unsqueeze(0).repeat(x.shape[0],1,1)
+
+    w = weight_layer.weight.repeat(x.shape[0],1,1)
+    return bound_f(w, h_n, jh_n), (w,h_n,jh_n)
+
+def skip_mlp_bound(weight_layer, partial_net, x, bound_f = calc_tight_bound):
+    negative_slope = weight_layer.activation.negative_slope
+
+    h_0 = x.view(x.shape[0],-1)
+    h_n = partial_net(h_0)
+    jh_n = vmap(jacrev(partial_net))(h_0)
+
+    w_base = weight_layer.fc.weight.repeat(x.shape[0],1,1)
+    w_skip =torch.eye(*w_base.shape[1:]).unsqueeze(0).repeat(x.shape[0],1,1)
+    w_skip = w_skip * (1 + (torch.gt(-weight_layer.fc(h_n), 0) * (negative_slope**(-1) - 1 ))).unsqueeze(1)
+    w = w_base + w_skip
+    return bound_f(w, h_n, jh_n), (w,h_n,jh_n)
+
+def get_bounds(model, x):
+    modules_list = list(model.layers._modules.items())
+    bound_types = {
+        'fc': mlp_bound,
+        'sk': skip_mlp_bound
+    }
+    layer_wise_bound = []
+    for i in range(len(modules_list)):
+        name, layer = modules_list[i]
+        if name[:2] in bound_types.keys():
+            bound, _ = bound_types[name[:2]](
+                layer,
+                nn.Sequential(OrderedDict(modules_list[:i])),
+                x
+            )
+            layer_wise_bound.append(bound)
+    return torch.stack(layer_wise_bound).T.detach()
+
+def compose_fns(f,g): return lambda x : g(f(x))
+
+def norm_grad_params(model):
+    return torch.tensor([(param.grad**2).sum() for param in model.parameters()]).sum()
+
+def norm_grad_x(model, loss_fn, x, labels):
+    grad_x = jacrev(compose_fns(model, partial(loss_fn,target=labels)))(x)
+    return (grad_x**2).sum().detach()
+
+def norm_grads(model, loss_fn, optimizer, x, labels):
+    optimizer.zero_grad()
+    inputs = x.clone()
+    inputs.requires_grad_(True)
+    inputs.retain_grad()
+
+    outputs = model(inputs)
+    loss = loss_fn(outputs, labels)
+    loss.backward()
+
+    norm_grad_wrt_input = (inputs.grad**2).sum()
+    norm_grad_wrt_params = torch.tensor([(param.grad**2).sum() for param in model.parameters()]).sum()
+    return (norm_grad_wrt_input,
+        norm_grad_wrt_params
+    )
--- start.py
+++ start.py
@@ -50,90 +50,7 @@ from models.simple_mlp import SimpleMLP
 from models.skip_mlp import SkipMLP
 from data.mnist import MNISTDataModule
 from utils import get_all_layers
-
-from functorch import jacrev, vmap
-
-def calc_bound(w, h, jh):
-    return (
-        (1 + linalg.vector_norm(h, dim=1)**2) /
-        (linalg.matrix_norm(w, ord=2)**2 *
-        linalg.matrix_norm(jh, ord='fro')**2)
-    )
-
-def calc_tight_bound(w, h, jh):
-    return (
-        (1 + linalg.vector_norm(h, dim=1)**2) /
-        (linalg.matrix_norm(torch.bmm(w, jh), ord='fro')**2)
-    )
-
-def mlp_bound(weight_layer, partial_net, x, bound_f = calc_tight_bound):
-    h_0 = x.view(x.shape[0],-1)
-    if len(partial_net) > 0:
-        h_n = partial_net(h_0)
-        jh_n = vmap(jacrev(partial_net))(h_0)
-    else:
-        h_n = h_0
-        jh_n = torch.eye(h_0.shape[1]).unsqueeze(0).repeat(x.shape[0],1,1)
-
-    w = weight_layer.weight.repeat(x.shape[0],1,1)
-    return bound_f(w, h_n, jh_n), (w,h_n,jh_n)
-
-def skip_mlp_bound(weight_layer, partial_net, x, bound_f = calc_tight_bound):
-    negative_slope = weight_layer.activation.negative_slope
-
-    h_0 = x.view(x.shape[0],-1)
-    h_n = partial_net(h_0)
-    jh_n = vmap(jacrev(partial_net))(h_0)
-
-    w_base = weight_layer.fc.weight.repeat(x.shape[0],1,1)
-    w_skip =torch.eye(*w_base.shape[1:]).unsqueeze(0).repeat(x.shape[0],1,1)
-    w_skip = w_skip * (1 + (torch.gt(-weight_layer.fc(h_n), 0) * (negative_slope**(-1) - 1 ))).unsqueeze(1)
-    w = w_base + w_skip
-    return bound_f(w, h_n, jh_n), (w,h_n,jh_n)
-
-def get_bounds(model, x):
-    modules_list = list(model.layers._modules.items())
-    bound_types = {
-        'fc': mlp_bound,
-        'sk': skip_mlp_bound
-    }
-    layer_wise_bound = []
-    for i in range(len(modules_list)):
-        name, layer = modules_list[i]
-        if name[:2] in bound_types.keys():
-            bound, _ = bound_types[name[:2]](
-                layer,
-                nn.Sequential(OrderedDict(modules_list[:i])),
-                x
-            )
-            layer_wise_bound.append(bound)
-    pdb.set_trace()
-    return torch.stack(layer_wise_bound).T
-
-def compose_fns(f,g): return lambda x : g(f(x))
-
-def norm_grad_params(model):
-    return torch.tensor([(param.grad**2).sum() for param in model.parameters()]).sum()
-
-def norm_grad_x(model, loss_fn, x, labels):
-    grad_x = jacrev(compose_fns(model, partial(loss_fn,target=labels)))(x)
-    return (grad_x**2).sum()
-
-def norm_grads(model, loss_fn, optimizer, x, labels):
-    optimizer.zero_grad()
-    inputs = x.clone()
-    inputs.requires_grad_(True)
-    inputs.retain_grad()
-
-    outputs = model(inputs)
-    loss = loss_fn(outputs, labels)
-    loss.backward()
-
-    norm_grad_wrt_input = (inputs.grad**2).sum()
-    norm_grad_wrt_params = torch.tensor([(param.grad**2).sum() for param in model.parameters()]).sum()
-    return (norm_grad_wrt_input,
-        norm_grad_wrt_params
-    )
+from func_geometric_bounds import norm_grad_x, norm_grad_params, get_bounds
 
 def train_one_epoch(training_loader, model, loss_fn,optimizer, epoch_index, tb_writer):
     running_loss = 0.
@@ -144,41 +61,17 @@ def train_one_epoch(training_loader, model, loss_fn,optimizer, epoch_index, tb_w
         optimizer.zero_grad()
         outputs = model(inputs)
         loss = loss_fn(outputs, labels)
-
         loss.backward()
 
-        #no need for vmap since loss fn has sum over batch
-        grad_wrt_input = jacrev(compose_fns(model, partial(loss_fn,target=labels)))(inputs)
-        norm_grad_wrt_input = (grad_wrt_input**2).sum()
-        print(torch.allclose(norm_grad_x(model, loss_fn, inputs, labels), norm_grad_wrt_input))
-        norm_grad_wrt_weights = torch.tensor([(param.grad**2).sum() for param in model.parameters()]).sum()
-        print(torch.allclose(norm_grad_wrt_weights, norm_grad_params(model)))
-        #
-        print(norm_grad_x(model, loss_fn, inputs, labels), norm_grad_params(model))
-        print(norm_grads(model, loss_fn, optimizer, inputs, labels))
-        #test = torch.autograd.functional.jacobian(model, inputs)
-        # def cfnew(f,g): return lambda x : f(g(x))
-        #  torch.autograd.functional.jacobian(cfnew(partial(loss_fn,target=labels), model), inputs)
-        # [torch.allclose(a,b) for (a,b) in zip(h_n, visualisation.values()[0:6:2])]
-
-
-        #assert [torch.allclose(a,b[0]) for (a,b) in zip(h_n, list(visualisation.values())[0:6:2])]
-        if 0:
-            h_n, jh_n = get_hn_and_jhn(model, inputs)
-            bound = old_mlp_bound(model,inputs)
-            tight_bound=tighter_mlp_bound(model,inputs)
-            print('f in', norm_grad_wrt_input)
-            print('bound', bound)
-            print('bound2', tight_bound)
-            print('f in * bound', norm_grad_wrt_input*bound)
-            print('Tighter f in * bound', norm_grad_wrt_input*tight_bound)
-            print('f param', norm_grad_wrt_weights)
-            #assert all([torch.allclose(a,b[0]) for (a,b) in zip(h_n, list(visualisation.values())[0:6:2])])
-
-        test = get_bounds(model,inputs)
-        print('\n\n new way to calc bounds')
-        print(test)
-        print(test.sum(dim=1))
+        # test bounds and gradients
+        bound = get_bounds(model, inputs).sum()
+        norm_grad_wrt_x = norm_grad_x(model, loss_fn, inputs, labels)
+        norm_grad_wrt_params = norm_grad_params(model)
+        print('Gradient with x:   ', norm_grad_wrt_x)
+        print('Bound:             ', bound)
+        print('Gradient with p:   ', norm_grad_wrt_params)
+        print(f"Is {bound*norm_grad_wrt_x} < {norm_grad_wrt_params}")
+        print(bound*norm_grad_wrt_x < norm_grad_wrt_params)
         pdb.set_trace()
         optimizer.step()
         # Gather data and report
@@ -191,6 +84,7 @@ def train_one_epoch(training_loader, model, loss_fn,optimizer, epoch_index, tb_w
             running_loss = 0.
 
     return last_loss
+
 
 
 def main_manual_train():
