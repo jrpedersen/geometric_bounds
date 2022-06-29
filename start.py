5d3e6ffce352e583d378913e61c0f7114da9e647
jrpedersen <jesper2@peders1.dk>
2022-06-29 15:12:02 +0200

    Moved old code out of start.

--- start.py
+++ start.py
@@ -110,43 +110,6 @@ def get_bounds(model, x):
     pdb.set_trace()
     return torch.stack(layer_wise_bound).T
 
-def get_hn_and_jhn(model, x):
-    h_0 = x.view(1,-1)
-
-    h_n = [h_0]
-    jh_0 = torch.eye(h_0.shape[1]).unsqueeze(0)
-    jh_n = [jh_0]
-
-    for n in range(1,3):
-        partial_net =nn.Sequential(OrderedDict(list(model.layers._modules.items())[:(n*2)]))
-        h_n.append(partial_net(h_0))
-        jh_n.append(torch.squeeze(jacrev(partial_net)(h_0)))
-    return h_n, jh_n
-
-def old_mlp_bound(model, x):
-    h_n, jh_n = get_hn_and_jhn(model, x)
-    params = [param for (i,param) in enumerate(model.parameters()) if i%2==0]
-    bound = 0
-    for w, h, jh in zip(params,h_n,jh_n):
-        bound += (
-            (1 + linalg.vector_norm(h)**2) /
-            (linalg.matrix_norm(w, ord=2)**2 *
-            linalg.matrix_norm(jh, ord='fro')**2)
-        )
-    return bound
-
-def tighter_mlp_bound(model, x):
-    h_n, jh_n = get_hn_and_jhn(model, x)
-    params = [param for (i,param) in enumerate(model.parameters()) if i%2==0]
-    bound = 0
-    for w, h, jh in zip(params,h_n,jh_n):
-        bound += (
-            (1 + linalg.vector_norm(h)**2) /
-            (linalg.matrix_norm(torch.matmul(w, jh), ord='fro')**2)
-        )
-    return bound
-
-
 def compose_fns(f,g): return lambda x : g(f(x))
 
 def norm_grad_params(model):
